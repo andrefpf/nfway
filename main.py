8e55d91a0de0574c8b75bab84101f7c524ba48be
André Fernandes <fpf.andre@gmail.com>
2025-09-05 20:48:14 -0300

    add basic cli functionality

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from nfway.scrapper import read_url
+from nfway.cli import cli_parser
+
 from pprint import pprint
 
+
 def main():
-    info = read_url("...")
-    pprint(info)
+    parser = cli_parser()
+    args = parser.parse_args()
+
+    result_value = None
+    if args.url is not None:
+        result_value = read_url(args.url)
+
+    elif args.qr_code is not None:
+        raise NotImplementedError("QR code reading not implemented yet.")
+
+    else:
+        print("No input provided.")
+        parser.print_help()
+        exit()
+
+    if args.output is not None:
+        raise NotImplementedError("Output to file not implemented yet.")
+
+    else:
+        pprint(result_value)
+
 
 if __name__ == "__main__":
     main()
---
+++ nfway/cli.py
@@ -0,0 +1,33 @@
+from nfway.scrapper import read_url
+from pprint import pprint
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def cli_parser() -> ArgumentParser:
+    parser = ArgumentParser(description="NF-Way CLI Tool.")
+
+    parser.add_argument(
+        "-u",
+        "--url",
+        type=str,
+        help="URL of the NF-e page to scrape",
+    )
+
+    parser.add_argument(
+        "-q",
+        "--qr-code",
+        type=Path,
+        help="Path to read the QR code image containing the NF-e URL",
+        default=None,
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        help="Output file to save the NF-e data",
+        default=None,
+    )
+
+    return parser
