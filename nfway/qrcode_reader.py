b21456aa39df0001a9e66876bee32b06f8655254
André Fernandes <fpf.andre@gmail.com>
2025-09-07 14:25:42 -0300

    Use QReader as a fallback QR code reader

--- nfway/qrcode_reader.py
+++ nfway/qrcode_reader.py
@@ -1,9 +1,19 @@
 import cv2
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+
 
 def read_qrcode(image_path: Path) -> str | None:
-    img = cv2.imread(image_path)
+    img = cv2.imread(str(image_path))
+
     detector = cv2.QRCodeDetector()
-    data, bbox, straight_qrcode = detector.detectAndDecode(img)
+    data, _, _ = detector.detectAndDecode(img)
+
+    if not data:
+        from qreader import QReader
+
+        qreader = QReader()
+        data, *_ = qreader.detect_and_decode(image=img)
+
     return data if data else None
