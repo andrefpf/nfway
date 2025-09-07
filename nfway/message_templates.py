c5ad882eb02bd96afc9d066336463e457ee90447
André Fernandes <fpf.andre@gmail.com>
2025-09-07 15:28:49 -0300

    Refactor telegram bot messages

---
+++ nfway/message_templates.py
@@ -0,0 +1,41 @@
+EMITTER_HEADER_TEMPLATE = """
+<b>{name}</b>
+<i>CNPJ: {cnpj}</i>
+
+<i>{address}</i>
+"""
+
+ITEM_TEMPLATE = """
+<b>Item:</b> {name}
+<b>Quantidade:</b> {quantity} {quantity_unit}
+<b>Valor unitário:</b> {unit_value}
+<b>Valor do item:</b> {item_value}
+"""
+
+NF_INFO_TEMPLATE = """
+<b>Número da NF:</b> {number}
+<b>Série da NF:</b> {series}
+<b>Chave de Acesso da NF:</b> {access_key}
+<b>Data de Emissão:</b> {emission_date}
+<b>Hora de Emissão:</b> {emission_time}
+"""
+
+TOTAL_TEMPLATE = """
+<b>Valor Total:</b> {total_value}
+<i>Total em Impostos:</i> {total_taxes}
+"""
+
+BAD_URL_TEMPLATE = """
+<b>Não foi possível ler a Nota Fiscal na URL informada.</b>
+Verifique se a URL pertence ao site oficial: https://sat.sef.sc.gov.br/.
+"""
+
+BAD_QR_CODE_TEMPLATE = """
+<b>Não foi possível identificar o QR code informado.</b>
+Por favor, tente novamente com uma imagem mais nítida.
+"""
+
+GOOD_QR_CODE_BAD_URL_TEMPLATE = """
+<b>O QR code foi lido corretamente, mas nao foi possivel identificar a Nota Fiscal.</b>
+Certifique-se de que o QR code pertence a uma Nota Fiscal.
+"""
--- nfway/telegram_bot.py
+++ nfway/telegram_bot.py
@@ -12,14 +12,54 @@ from telegram.ext import (
 
 from tempfile import TemporaryFile
 
+from nfway.nf_info import NFInfo
 from nfway.scrapper import read_url
 from nfway.qrcode_reader import read_qrcode
+from nfway import message_templates
 
 # Enable logging
 fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(format=fmt, level=logging.INFO)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
+
+
+def emitter_message(nf: NFInfo) -> str:
+    return message_templates.EMITTER_HEADER_TEMPLATE.format(
+        name=nf.emiter_name,
+        cnpj=nf.emiter_cnpj,
+        address=nf.emiter_address,
+    )
+
+
+def nf_info_message(nf: NFInfo) -> str:
+    return message_templates.NF_INFO_TEMPLATE.format(
+        number=nf.number,
+        series=nf.series,
+        access_key=nf.access_key,
+        emission_date=nf.emission_date,
+        emission_time=nf.emission_time,
+    )
+
+
+def list_items_message(nf: NFInfo) -> str:
+    message = ""
+
+    for item in nf.items:
+        message += message_templates.ITEM_TEMPLATE.format(
+            name=item.name,
+            quantity=item.quantity,
+            quantity_unit=item.quantity_unit,
+            unit_value=item.unit_value,
+            item_value=item.item_value,
+        )
+
+    message += message_templates.TOTAL_TEMPLATE.format(
+        total_value=nf.total_value,
+        total_taxes=nf.total_taxes,
+    )
+
+    return message
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -33,15 +73,21 @@ async def handle_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     logger.info(f"User {user.first_name} sent a link")
 
     try:
-        result_value = read_url(update.message.text)
+        nf_result = read_url(update.message.text)
+
     except Exception as e:
         print_exception(e)
+        await update.message.reply_html(message_templates.BAD_URL_TEMPLATE)
 
-        response_text = "Não foi possível ler a Nota Fiscal.\n"
-        response_text += "Verifique se o link informado pertence ao site oficial: https://sat.sef.sc.gov.br/."
-        await update.message.reply_text(response_text)
     else:
-        await update.message.reply_text(str(result_value))
+        await update.message.reply_html(emitter_message(nf_result))
+        if nf_result.emiter_coords is not None:
+            await update.message.reply_location(
+                latitude=nf_result.emiter_coords[0],
+                longitude=nf_result.emiter_coords[1],
+            )
+        await update.message.reply_html(nf_info_message(nf_result))
+        await update.message.reply_html(list_items_message(nf_result))
 
 
 async def handle_qr_code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -52,26 +98,38 @@ async def handle_qr_code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
     photo_file = await update.message.photo[-1].get_file()
     await photo_file.download_to_drive(download_path)
 
-    url = read_qrcode(download_path)
+    try:
+        url = read_qrcode(download_path)
+
+    except Exception as e:
+        print_exception(e)
+        await update.message.reply_html(message_templates.BAD_QR_CODE_TEMPLATE)
+        return
+
     if url is None:
-        response_text = "Não foi possível identificar o QR code informado.\n"
-        response_text += "Por favor, tente novamente com uma imagem mais nítida."
-        await update.message.reply_text(response_text)
+        await update.message.reply_html(message_templates.BAD_QR_CODE_TEMPLATE)
         return
 
     try:
-        result_value = read_url(url)
+        nf_result = read_url(url)
+
     except Exception as e:
         print_exception(e)
+        await update.message.reply_html(message_templates.GOOD_QR_CODE_BAD_URL_TEMPLATE)
 
-        response_text = "O correto QR code foi identificado, mas nao foi possivel ler a Nota Fiscal."
-        await update.message.reply_text(response_text)
     else:
-        await update.message.reply_text(str(result_value))
+        await update.message.reply_html(emitter_message(nf_result))
+        if nf_result.emiter_coords is not None:
+            await update.message.reply_location(
+                latitude=nf_result.emiter_coords[0],
+                longitude=nf_result.emiter_coords[1],
+            )
+        await update.message.reply_html(nf_info_message(nf_result))
+        await update.message.reply_html(list_items_message(nf_result))
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Envie um QR code ou o link de uma Nota Fiscal.")
+async def default(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Envie um *QR code* ou o link de uma Nota Fiscal.")
 
 
 def initialize(token: str) -> None:
@@ -80,10 +138,11 @@ def initialize(token: str) -> None:
     handlers = [
         CommandHandler("start", start),
         MessageHandler(
-            filters.Entity("url") | filters.Entity("text_link"), handle_link
+            filters.Entity("url") | filters.Entity("text_link"),
+            handle_link,
         ),
         MessageHandler(filters.PHOTO, handle_qr_code),
-        MessageHandler(filters.TEXT & ~filters.COMMAND, echo),
+        MessageHandler(filters.TEXT & ~filters.COMMAND, default),
     ]
 
     app.add_handlers(handlers)
